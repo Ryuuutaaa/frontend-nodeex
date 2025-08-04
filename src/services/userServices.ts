9d0dd1ffdf56479402f51ca46ac2edb321258b9f
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-08-04 20:56:03 +0800

    feat : add fitru update data by id

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -63,3 +63,29 @@ export const getUserById = async (id: string): Promise<User> => {
     throw error;
   }
 };
+
+export const updateUserById = async (
+  id: string,
+  updates: Partial<Omit<User, "id">>
+): Promise<string> => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updates),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Gagal memperbarui data pengguna");
+    }
+
+    const message = await response.text();
+    return message;
+  } catch (error) {
+    console.error("Error update user by id", error);
+    throw error;
+  }
+};
