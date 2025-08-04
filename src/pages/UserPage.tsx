5634c18dea879d1728827e4cd60b8d26d0078c30
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-08-04 22:40:48 +0800

    feat : add usePage

---
+++ src/pages/UserPage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface User {
+  id: string;
+  firstsname: string;
+  lastname: string;
+  age: number;
+}
+
+interface newUser {
+  firstsname: string;
+  lastname: string;
+  age: number;
+}
+
+const UserPage = () => {
+  return <div>UserPage</div>;
+};
+
+export default UserPage;
